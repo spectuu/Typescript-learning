39d04f7d57bb87573ecaa67c3e7f0a14304e97af
Spectu <Santymartinez913@gmail.com>
2020-11-25 12:46:40 -0500

    typescript learnings

---
+++ scripts.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+//types
+var myString = "Hello World";
+myString = 22 + "";
+var Mynumber = 22;
+var Myboolean = true;
+var MyVar = "V";
+MyVar = false;
+//strings 
+document.write(myString);
+//arrays
+var Array1 = ["", "", ""];
+Array1 = [1, 2, 3];
+var StringArry = ["item1", "item2", ""];
+var NumberArray = [1, 2, 3, 4];
+var booleanArray = [false, true, true];
+var AnyArray = [false, "Hello", [], {}];
---
+++ scripts.ts
@@ -0,0 +1,31 @@
+console.log("Hello World");
+
+//types
+
+var myString: string = "Hello World";
+myString= 22 + "";
+
+var Mynumber = 22;
+var Myboolean: boolean = true;
+var MyVar: any = "V";
+MyVar = false;
+
+//strings 
+
+document.write(myString);
+
+//arrays
+
+var Array1: any[] = ["", "", ""];
+Array1 = [1,2,3];
+
+var StringArry: string[] = ["item1", "item2", ""];
+var NumberArray: number[] = [1, 2, 3, 4];
+var booleanArray: boolean[] = [false, true, true];
+var AnyArray: any[] = [false, "Hello", [], {}];
+
+//tuples
+
+var StringTuple: [string, number]
+StringTuple = ["hello", 22]
+
